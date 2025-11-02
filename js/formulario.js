72108b5152fa536b60ebfed05ffc71573a762f41
Springboybnuy <lutorresgarcia8@gmail.com>
2025-11-01 23:10:05 -0300

    patterns para forms

--- js/formulario.js
+++ js/formulario.js
@@ -1,1 +1,8 @@
-{\rtf1}
+  const form = document.querySelector(".forms");
+
+  const name = form.querySelector('input[name="name"]').value;
+  if (!/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/.test(name)) {
+    e.preventDefault();
+    alert("El nombre solo puede contener letras y espacios");
+    return;
+  }
